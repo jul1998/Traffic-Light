dc945a472a369a00d3e8a5b3622a8e666febe1d0
jul1998 <jguevara321@gmail.com>
2022-10-01 07:30:08 +0000

    First commit

---
+++ src/js/component/TrafficLight.jsx
@@ -0,0 +1,53 @@
+
+import React, {useState} from "react";
+
+export default function TrafficLight(){
+    const [color, setLight] = React.useState(str)
+    var str = ""
+    function showLightRed(){
+        str = "red"
+        setLight(str)
+    }
+    function showLightYellow(){
+        str = "yellow"
+        setLight(str)
+    }
+    
+    function showLightGreen(){
+        str = "green"
+        setLight(str)
+    }
+    
+    const colors = ["red","yellow", "green"]
+
+    function cycle(){
+        let count = 0
+        setInterval(() => {
+            if(count !== 3){
+                str = colors[count]
+                setLight(str)
+                console.log(str)
+                count++
+            }else{
+                count =0
+            }
+               
+        }, 1000);
+    }
+
+
+    
+    return(
+        <>
+
+
+        <div className="main-trafic">
+            <div onClick={showLightRed} className={"red-light" +(color=== "red"? "-glow": "")}></div>
+            <div onClick={showLightYellow} className={"yellow-light" + (color === "yellow"? "-glow":"")}></div>
+            <div onClick={showLightGreen} className={"green-light" + (color === "green"? "-glow":"")}></div>
+            <button onClick={cycle}>Click Me</button>
+
+        </div>
+        </>
+    )
+}
